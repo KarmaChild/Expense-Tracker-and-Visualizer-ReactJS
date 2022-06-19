e242a882eae00cc3e21401af5cdf71f48269945a
adith <adm115@mail.usask.ca>
2022-06-19 13:04:33 -0600

    graphql init

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+
+
+
+const app = express()
+
+app.get('/hello',(req,res) =>{
+    res.send('hello world')
+})
+
+
+app.listen(8000, () =>{
+    console.log('🚀 server running')
+})
