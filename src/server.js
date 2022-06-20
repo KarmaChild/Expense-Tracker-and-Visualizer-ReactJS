0d20a05dd95ad3689cd8cc1c386b2e0e6ce725de
adith <adm115@mail.usask.ca>
2022-06-20 17:41:26 -0600

    DB connected

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 
 
-
+const url = "mongodb+srv://mongo:fStqR3fnpNrABU_@cluster0.ya30d.mongodb.net/?retryWrites=true&w=majority"
 const app = express()
+
+
+const connection = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(url,connection).then(()=>{
+    console.info("💯 Connected to DB")
+}).catch((error)=>{
+    console.log("❌ Error:",error)
+})
 
 app.get('/hello',(req,res) =>{
     res.send('hello world')
